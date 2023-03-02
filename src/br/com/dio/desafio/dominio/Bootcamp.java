c667ca3c17239c918a930d682336bf17617c2e85
João Loureiro <joao@t2s.com.br>
2023-03-01 21:43:04 -0300

    Adicionado classes Bootcamp e Dev

---
+++ src/br/com/dio/desafio/dominio/Bootcamp.java
@@ -0,0 +1,62 @@
+package br.com.dio.desafio.dominio;
+
+import java.time.LocalDate;
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
+
+public class Bootcamp {
+    private static final int DIAS_PADRAO_BOOTCAMP = 45;
+
+    private String nome;
+    private String descricao;
+    private LocalDate dataInicial = LocalDate.now();
+    private LocalDate dataFinal = dataInicial.plusDays(DIAS_PADRAO_BOOTCAMP);
+    private Set<Dev> devsIncritos = new LinkedHashSet<>();
+    private Set<Conteudo> conteudos = new LinkedHashSet<>();
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Set<Dev> getDevsIncritos() {
+        return devsIncritos;
+    }
+
+    public void setDevsIncritos(Set<Dev> devsIncritos) {
+        this.devsIncritos = devsIncritos;
+    }
+
+    public Set<Conteudo> getConteudos() {
+        return conteudos;
+    }
+
+    public void setConteudos(Set<Conteudo> conteudos) {
+        this.conteudos = conteudos;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Bootcamp bootcamp = (Bootcamp) o;
+        return Objects.equals(nome, bootcamp.nome) && Objects.equals(descricao, bootcamp.descricao) && Objects.equals(dataInicial, bootcamp.dataInicial) && Objects.equals(dataFinal, bootcamp.dataFinal) && Objects.equals(devsIncritos, bootcamp.devsIncritos) && Objects.equals(conteudos, bootcamp.conteudos);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, descricao, dataInicial, dataFinal, devsIncritos, conteudos);
+    }
+}
